d09732dd2d054bb85f72196c04413ab28f18d836
Eagle <ii-sys@mail.ru>
2024-11-12 14:25:00 +0700

    2-4

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -8,7 +8,7 @@ app.config['SECRET_KEY'] = os.getenv('a242752b0e9a71335718af7cbebfdd6d')
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to Flask!'
+    return render_template('html/index.html')
 
 
 '''@app.get('/')
