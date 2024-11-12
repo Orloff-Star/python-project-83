398517985b0bef71fd4dd0b4487aafcde697480d
Eagle <ii-sys@mail.ru>
2024-11-12 13:55:35 +0700

    step 2, version 3

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -6,6 +6,11 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('a242752b0e9a71335718af7cbebfdd6d')
 
+'''@app.route('/')
+def hello_world():
+    return 'Welcome to Flask!'''
+
+
 @app.get('/')
 def page_analyzer():
-    return render_template('index.html')
+    return render_template('html/index.html')
