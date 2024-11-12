24e68f3c7aff7608ffd0ee70e4552a5f02dc2885
Eagle <ii-sys@mail.ru>
2024-11-12 14:49:51 +0700

    2-5

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -8,7 +8,7 @@ app.config['SECRET_KEY'] = os.getenv('a242752b0e9a71335718af7cbebfdd6d')
 
 @app.route('/')
 def hello_world():
-    return render_template('html/index.html')
+    return render_template('index.html')
 
 
 '''@app.get('/')
