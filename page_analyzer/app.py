75ea2328d18cd71e301f5475a152fef63a8c0a6d
Eagle <ii-sys@mail.ru>
2024-12-05 11:50:21 +0700

    проверка третья

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -11,6 +11,7 @@ from page_analyzer import db, functions
 load_dotenv()
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["DATABASE_URL"] = os.getenv('DATABASE_URL')
 
 
 @app.errorhandler(404)
@@ -31,10 +32,11 @@ def get_home_page():
 
 @app.get("/urls/<url_id>")
 def get_page_url(url_id):
-    url_info = db.get_url(url_id)
-    url_checks = db.get_url_check(url_id)
-    if not url_info:
-        abort(404)
+    with db.get_connection(app.config["DATABASE_URL"]) as conn:
+        url_info = db.get_url(conn, url_id)
+        url_checks = db.get_url_check(conn, url_id)
+        if not url_info:
+            abort(404)
     return render_template(
         "url.html",
         url=url_info,
@@ -44,41 +46,47 @@ def get_page_url(url_id):
 
 @app.post("/urls")
 def post_url():
-    url_name = request.form.get("url")
-    errors = functions.validate(url_name)
-    if errors:
-        for error in errors.values():
-            flash(error, "error")
-        return render_template("index.html", url_error=url_name), 422
-    valid_url = functions.normalize_url(url_name)
-    url = db.get_url_by_name(valid_url)
-    if url:
-        flash("Страница уже существует", "info")
-        return redirect(url_for("get_page_url", url_id=url.id), code=302)
-    url = db.add_url(valid_url)
-    flash("Страница успешно добавлена", "success")
+    with db.get_connection(app.config["DATABASE_URL"]) as conn:
+        url_name = request.form.get("url")
+        errors = functions.validate(url_name)
+        if errors:
+            for error in errors.values():
+                flash(error, "error")
+            return render_template("index.html", url_error=url_name), 422
+        valid_url = functions.normalize_url(url_name)
+        url = db.get_url_by_name(conn, valid_url)
+        if url:
+            flash("Страница уже существует", "info")
+            return redirect(url_for("get_page_url", url_id=url.id), code=302)
+        url = db.add_url(conn, valid_url)
+        flash("Страница успешно добавлена", "success")
     return redirect(url_for("get_page_url", url_id=url))
 
 
 @app.get("/urls")
 def get_list_urls():
-    urls = db.get_urls()
-    url_checks = {item.url_id: item for item in db.get_last_url_check()}
+    with db.get_connection(app.config["DATABASE_URL"]) as conn:
+        urls = db.get_urls(conn)
+        url_checks = db.get_last_url_checks(conn)
     return render_template("urls.html", urls=urls, url_checks=url_checks)
 
 
 @app.post("/urls/<url_id>/checks")
 def url_checks(url_id):
-    url_info = db.get_url(url_id)
-    try:
-        response = requests.get(url_info.name)
-        response.raise_for_status()
-    except requests.RequestException:
-        flash("Произошла ошибка при проверке", "error")
-        return redirect(url_for("get_page_url", url_id=url_id))
-    h1, title, description = functions.get_site_data(response.text)
-    db.add_url_check(url_id, response.status_code, h1, title, description)
-    flash("Страница успешно проверена", "success")
+    with db.get_connection(app.config["DATABASE_URL"]) as conn:
+        url_info = db.get_url(conn, url_id)
+
+        try:
+            response = requests.get(url_info.name)
+            response.raise_for_status()
+        except requests.RequestException:
+            flash("Произошла ошибка при проверке", "error")
+            return redirect(url_for("get_page_url", url_id=url_id))
+
+        h1, title, description = functions.get_site_data(response.text)
+        db.add_url_check(conn, url_id, response.status_code, h1,
+                         title, description)
+        flash("Страница успешно проверена", "success")
     return redirect(url_for("get_page_url", url_id=url_id))
 
 
--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,76 +1,66 @@
-import os
 from datetime import datetime
 
 import psycopg2
-from dotenv import load_dotenv
 from psycopg2.extras import NamedTupleCursor
 
-load_dotenv()
-DATABASE_URL = os.getenv('DATABASE_URL')
+
+def get_connection(db):
+    return psycopg2.connect(db)
 
 
-def get_connection():
-    return psycopg2.connect(DATABASE_URL)
+def get_url(conn, url_id):
+    with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+        cur.execute('SELECT * FROM urls WHERE id=%s', (url_id,))
+        return cur.fetchone()
 
 
-def get_url(url_id):
-    with get_connection() as conn:
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
-            cur.execute('SELECT * FROM urls WHERE id=%s', (url_id,))
-            return cur.fetchone()
+def get_url_check(conn, url_id):
+    with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+        cur.execute('SELECT * FROM url_checks WHERE url_id=%s', (url_id,))
+        return cur.fetchall()
 
 
-def get_url_check(url_id):
-    with get_connection() as conn:
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
-            cur.execute('SELECT * FROM url_checks WHERE url_id=%s', (url_id,))
-            return cur.fetchall()
+def get_url_by_name(conn, url_name):
+    with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+        cur.execute('SELECT * FROM urls WHERE name=%s', (url_name,))
+        return cur.fetchone()
 
 
-def get_url_by_name(url_name):
-    with get_connection() as conn:
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
-            cur.execute('SELECT * FROM urls WHERE name=%s', (url_name,))
-            return cur.fetchone()
-
-
-def add_url(url_name):
+def add_url(conn, url_name):
     current_date = datetime.now()
-    with get_connection() as conn:
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
-            cur.execute('''INSERT INTO urls (name, created_at)
-                        VALUES (%s,%s) RETURNING id''',
-                        (url_name, current_date,))
-            return cur.fetchone()[0]
+    with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+        cur.execute('''INSERT INTO urls (name, created_at)
+                    VALUES (%s,%s) RETURNING id''',
+                    (url_name, current_date,))
+        return cur.fetchone()[0]
 
 
-def get_urls():
-    with get_connection() as conn:
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
-            cur.execute('SELECT * FROM urls')
-            return cur.fetchall()
+def get_urls(conn):
+    with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+        cur.execute('SELECT * FROM urls')
+        return cur.fetchall()
 
 
-def get_last_url_check():
-    with get_connection() as conn:
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
-            cur.execute('''SELECT DISTINCT ON (url_id) url_id,
-                        MAX(created_at) AS created_at,
-                        status_code
-                        FROM url_checks
-                        GROUP BY url_id, status_code
-                        ''')
-            return cur.fetchall()
+def get_last_url_checks(conn):
+    with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+        cur.execute('''SELECT DISTINCT ON (url_id) url_id,
+                    MAX(created_at) AS created_at,
+                    status_code
+                    FROM url_checks
+                    GROUP BY url_id, status_code
+                    ''')
+        cur.fetchall()
+        url_checks = {item.url_id: item for item in cur}
+        return url_checks
 
 
-def add_url_check(url_id, status_code, h1, title, description):
+def add_url_check(conn, url_id, status_code, h1, title, description):
     current_date = datetime.now()
-    with get_connection() as conn:
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
-            cur.execute('''INSERT INTO url_checks
-                        (url_id, status_code, h1, title,
-                        description, created_at)
-                        VALUES (%s, %s, %s, %s, %s, %s)''',
-                        (url_id, status_code, h1, title,
-                         description, current_date,)
-                        )
+    with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+        cur.execute('''INSERT INTO url_checks
+                    (url_id, status_code, h1, title,
+                     description, created_at)
+                     VALUES (%s, %s, %s, %s, %s, %s)''',
+                    (url_id, status_code, h1, title,
+                     description, current_date,)
+                    )
