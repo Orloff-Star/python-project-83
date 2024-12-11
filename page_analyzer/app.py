12dce35cf9b4ec527b84323243bc9633c889a23e
Eagle <ii-sys@mail.ru>
2024-12-11 11:30:34 +0700

    final checks

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from flask import (Flask, abort, flash, redirect, render_template, request,
                    url_for)
 
-from page_analyzer import db, functions
+from page_analyzer import data_normaliz, db
 
 load_dotenv()
 app = Flask(__name__)
@@ -21,7 +21,7 @@ def page_not_found(e):
 
 @app.errorhandler(500)
 def server_error(e):
-    logging.exception(str(e))
+    logging.error(str(e))
     return render_template("500.html"), 500
 
 
@@ -33,13 +33,13 @@ def get_home_page():
 @app.get("/urls/<url_id>")
 def get_page_url(url_id):
     with db.get_connection(app.config["DATABASE_URL"]) as conn:
-        url_info = db.get_url(conn, url_id)
+        url = db.get_url(conn, url_id)
         url_checks = db.get_url_check(conn, url_id)
-        if not url_info:
+        if not url:
             abort(404)
     return render_template(
         "url.html",
-        url=url_info,
+        url=url,
         url_checks=url_checks,
     )
 
@@ -48,12 +48,12 @@ def get_page_url(url_id):
 def post_url():
     with db.get_connection(app.config["DATABASE_URL"]) as conn:
         url_name = request.form.get("url")
-        errors = functions.validate(url_name)
+        errors = data_normaliz.validate(url_name)
         if errors:
             for error in errors.values():
                 flash(error, "error")
             return render_template("index.html", url_error=url_name), 422
-        valid_url = functions.normalize_url(url_name)
+        valid_url = data_normaliz.normalize_url(url_name)
         url = db.get_url_by_name(conn, valid_url)
         if url:
             flash("Страница уже существует", "info")
@@ -74,16 +74,16 @@ def get_list_urls():
 @app.post("/urls/<url_id>/checks")
 def url_checks(url_id):
     with db.get_connection(app.config["DATABASE_URL"]) as conn:
-        url_info = db.get_url(conn, url_id)
+        url = db.get_url(conn, url_id)
 
         try:
-            response = requests.get(url_info.name)
+            response = requests.get(url.name)
             response.raise_for_status()
         except requests.RequestException:
             flash("Произошла ошибка при проверке", "error")
             return redirect(url_for("get_page_url", url_id=url_id))
 
-        h1, title, description = functions.get_site_data(response.text)
+        h1, title, description = data_normaliz.get_site_data(response.text)
         db.add_url_check(conn, url_id, response.status_code, h1,
                          title, description)
         flash("Страница успешно проверена", "success")
